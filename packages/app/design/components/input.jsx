2165491db07b9529707a9b13884c9a01538379b1
a-down <136649391+a2downing@users.noreply.github.com>
2024-02-15 16:51:44 -0600

    updated login form and components

--- packages/app/design/components/input.jsx
+++ packages/app/design/components/input.jsx
@@ -1,4 +1,7 @@
 import { TextInput as NativeInput } from 'react-native'
 import { styled } from 'nativewind'
 
-export const TextInput = styled(NativeInput, 'p-2 bg-slate-100 rounded-md')
+export const TextInput = styled(
+  NativeInput,
+  'px-2.5 py-3 bg-slate-100 rounded-md',
+)
--- packages/app/features/login/login-screen.jsx
+++ packages/app/features/login/login-screen.jsx
@@ -8,10 +8,10 @@ import { Button } from 'app/design/components/button'
 export const LoginScreen = () => {
   return (
     <View className="flex-1 items-center justify-center bg-pink-500 p-12">
-      <View className="flex w-full max-w-[400px] gap-2 rounded-2xl border-2 border-pink-800 bg-slate-100/80 p-8">
+      <View className="flex w-full max-w-[400px] gap-2 rounded-2xl border border-pink-600 bg-slate-100/80 p-8 py-10">
         <H1>Login</H1>
         <Text>Enter your email to login or signup</Text>
-        <TextInput placeholder="Email" className="mb-8 mt-4" />
+        <TextInput placeholder="Email" className="mb-12 mt-8" />
         <Link href={'/home'}>
           <Button>Click Me</Button>
         </Link>
