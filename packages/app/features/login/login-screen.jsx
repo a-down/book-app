d0a2593ad9b3217d78ffa2b131265b5ac4aef698
a-down <136649391+a2downing@users.noreply.github.com>
2024-02-15 17:00:03 -0600

    removed unnessary tailwind class

--- packages/app/features/login/login-screen.jsx
+++ packages/app/features/login/login-screen.jsx
@@ -9,11 +9,7 @@ import { Form } from 'app/design/components/Form'
 export const LoginScreen = () => {
   return (
     <View className="flex-1 items-center justify-center bg-pink-500 p-12">
-      <Form
-        style={
-          'flex w-full max-w-[400px] gap-2 rounded-2xl border border-pink-600 bg-slate-100/80 p-8 py-10'
-        }
-      >
+      <Form style="w-full max-w-[400px] gap-2 rounded-2xl border border-pink-600 bg-slate-100/80 p-8 py-10">
         <H1>Login</H1>
         <Text>Enter your email to login or signup</Text>
         <TextInput placeholder="Email" className="mb-12 mt-8" />
