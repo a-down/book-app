a65e8ce01a6448ecc04ea204b255b203bc3952be
a-down <136649391+a2downing@users.noreply.github.com>
2024-02-15 17:02:51 -0600

    refactored Form component | tw for tailwind props

--- packages/app/design/components/Form.jsx
+++ packages/app/design/components/Form.jsx
@@ -2,8 +2,9 @@ import { View } from 'app/design/view'
 import { styled } from 'nativewind'
 import { Platform } from 'react-native'
 
-export const Form = ({ children, style }) => {
+export const Form = ({ children, tw }) => {
+  const style = 'flex flex-col'
   if (Platform.OS === 'web')
-    return <form className={`${style} flex flex-col`}>{children}</form>
-  return styled(View)
+    return <form className={`${tw} + ${style}`}>{children}</form>
+  return styled(View, style)
 }
