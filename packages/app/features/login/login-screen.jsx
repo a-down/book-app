3a081571dd78f3d61f807a9ca9157436668436db
a-down <136649391+a2downing@users.noreply.github.com>
2024-02-15 16:46:48 -0600

    updated component structure

--- packages/app/features/login/login-screen.jsx
+++ packages/app/features/login/login-screen.jsx
@@ -2,8 +2,8 @@ import { createParam } from 'solito'
 import { Link } from 'solito/link'
 import { H1, H3, P, Text, TextLink } from 'app/design/typography'
 import { View } from 'app/design/view'
-import { TextInput } from 'app/design/input'
-import { Button } from 'app/design/button'
+import { TextInput } from 'app/design/components/input'
+import { Button } from 'app/design/components/button'
 
 export const LoginScreen = () => {
   return (
@@ -11,7 +11,7 @@ export const LoginScreen = () => {
       <View className="flex w-full max-w-[400px] gap-2 rounded-2xl border-2 border-pink-800 bg-slate-100/80 p-8">
         <H1>Login</H1>
         <Text>Enter your email to login or signup</Text>
-        <TextInput placeholder="Email" />
+        <TextInput placeholder="Email" className="mb-8 mt-4" />
         <Link href={'/home'}>
           <Button>Click Me</Button>
         </Link>
