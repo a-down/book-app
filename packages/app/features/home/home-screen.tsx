d7b54167c0593623e0977daa852056ca08b6564f
a-down <136649391+a2downing@users.noreply.github.com>
2024-02-15 20:17:41 -0600

    currently reading card

--- packages/app/features/home/home-screen.tsx
+++ packages/app/features/home/home-screen.tsx
@@ -1,69 +1,67 @@
-import { A, H1, P, Text, TextLink } from 'app/design/typography'
-import { Row } from 'app/design/layout'
+import { A, H1, H2, H3, P, Text, TextLink } from 'app/design/typography'
+import { Row, Column } from 'app/design/layout'
 import { View } from 'app/design/view'
+import { Link } from 'solito/link'
 
 import { MotiLink } from 'solito/moti'
 
 export function HomeScreen() {
   return (
-    <View className="max-w-[400px] flex-1 items-center justify-center p-3">
-      <H1>Welcome to the Book App.</H1>
-      <View className="max-w-xl">
-        <P className="text-center">
-          Here is a basic starter to show you how you can navigate from one
-          screen to another. This screen uses the same code on Next.js and React
-          Native.
-        </P>
-        <P className="text-center">
-          Solito is made by{' '}
-          <A
-            href="https://twitter.com/fernandotherojo"
-            hrefAttrs={{
-              target: '_blank',
-              rel: 'noreferrer',
-            }}
-          >
-            Fernando Rojo
-          </A>
-          .
-        </P>
-        <P className="text-center">
-          NativeWind is made by{' '}
-          <A
-            href="https://twitter.com/mark__lawlor"
-            hrefAttrs={{
-              target: '_blank',
-              rel: 'noreferrer',
-            }}
-          >
-            Mark Lawlor
-          </A>
-          .
-        </P>
-      </View>
-      <View className="h-[32px]" />
-      <Row className="space-x-8">
-        <TextLink href="/user/fernando">Regular Link</TextLink>
-        <MotiLink
-          href="/user/fernando"
-          animate={({ hovered, pressed }) => {
-            'worklet'
+    <View className="flex-1 items-center bg-rose-400 p-4">
+      <View className="max-w-[500px] flex-1 items-stretch justify-start gap-8">
+        <Row className="items-end justify-between">
+          <View className="mt-8">
+            <H2>Welcome back...</H2>
+            <H1>Brittany!</H1>
+          </View>
+          <View className="absolute bottom-4 right-0 h-16 w-16 rounded-full bg-rose-700"></View>
+          <View className="absolute bottom-0 right-10 h-16 w-16 rounded-full bg-rose-600"></View>
+        </Row>
 
-            return {
-              scale: pressed ? 0.95 : hovered ? 1.1 : 1,
-              rotateZ: pressed ? '0deg' : hovered ? '-3deg' : '0deg',
-            }
-          }}
-          transition={{
-            type: 'timing',
-            duration: 150,
-          }}
-        >
-          <Text selectable={false} className="text-base font-bold">
-            Moti Link
-          </Text>
-        </MotiLink>
-      </Row>
+        <View className="relative overflow-hidden rounded-lg border border-rose-500 bg-rose-300/40 p-4 sm:p-6 lg:p-8">
+          <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-rose-500 via-rose-600 to-rose-700"></span>
+
+          <P className="mt-1 text-xs font-medium text-rose-950">
+            Currently reading...
+          </P>
+
+          <Row className="mt-2 gap-4">
+            <View>
+              <img
+                alt=""
+                src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                className="h-24 w-16 rounded-lg object-cover shadow-sm"
+              />
+            </View>
+            <Column className="w-full justify-end">
+              <H3 className="text-wrap text-lg font-bold text-gray-900">
+                Building a SaaS product as a software developer
+              </H3>
+
+              <P className="text-xs font-medium text-rose-900">John Doe</P>
+            </Column>
+          </Row>
+
+          <Row className="mt-4 h-8 w-full overflow-clip rounded-lg bg-rose-200">
+            <Row className=" h-8 w-[75%] rounded-r-lg bg-rose-800"></Row>
+          </Row>
+
+          {/* <Link href="#">
+            <Row className="mt-2 justify-center gap-1">
+              <View className="h-4 w-4 rounded-md bg-rose-800"></View>
+              <P className="text-xs font-medium text-rose-800">
+                Track Your Progress
+              </P>
+            </Row>
+          </Link> */}
+          <TextLink
+            className="mt-1 text-center text-sm font-medium text-rose-900"
+            href="#"
+          >
+            Track Your Progress
+          </TextLink>
+        </View>
+      </View>
     </View>
   )
 }
--- packages/app/features/login/login-screen.jsx
+++ packages/app/features/login/login-screen.jsx
@@ -8,8 +8,8 @@ import { Form } from 'app/design/components/Form'
 
 export const LoginScreen = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-pink-500 p-12">
-      <Form tw="w-full max-w-[400px] gap-2 rounded-2xl border border-pink-600 bg-slate-100/80 p-8 py-10">
+    <View className="flex-1 items-center justify-center bg-pink-500 p-4">
+      <Form tw="w-full max-w-[500px] gap-2 rounded-2xl border border-pink-600 bg-slate-100/80 p-8 py-10">
         <H1>Login</H1>
         <Text>Enter your email to login or signup</Text>
         <TextInput placeholder="Email" className="mb-12 mt-8" />
