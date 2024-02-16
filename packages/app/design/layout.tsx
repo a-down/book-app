2f1ee8504e4d829b01b631370a256af6f6146f86
a-down <136649391+a2downing@users.noreply.github.com>
2024-02-15 20:50:20 -0600

    added ScrollView element

--- packages/app/design/layout.tsx
+++ packages/app/design/layout.tsx
@@ -1,5 +1,10 @@
-import { View } from 'react-native'
+import { ScrollView as NativeScrollView, View, Platform } from 'react-native'
 import { styled } from 'nativewind'
 
 export const Row = styled(View, 'flex flex-row')
 export const Column = styled(View, 'flex flex-col')
+
+export const ScrollView =
+  Platform.OS === 'web'
+    ? styled(View, 'flex-1')
+    : styled(NativeScrollView, 'flex-1')
