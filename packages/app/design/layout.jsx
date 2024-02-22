cc692846ee937ed0c5a30006f87e7e76616b3122
a-down <136649391+a2downing@users.noreply.github.com>
2024-02-21 21:12:30 -0600

    expo opens without error

--- packages/app/design/layout.jsx
+++ packages/app/design/layout.jsx
@@ -4,7 +4,11 @@ import { styled } from 'nativewind'
 export const Row = styled(View, 'flex flex-row items-center')
 export const Column = styled(View, 'flex flex-col')
 
+const NativeScrollViewWithFlex = () => (
+  <NativeScrollView contentContainerStyle={{ flex: 1 }} />
+)
+
 export const ScrollView =
   Platform.OS === 'web'
     ? styled(View, 'flex-1')
-    : styled(NativeScrollView, 'flex-1')
+    : styled(NativeScrollViewWithFlex)
